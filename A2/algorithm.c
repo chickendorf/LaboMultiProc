c502345ee5074dcde75be1703ae2d39f8e36e0bc
Arthur Vernet <arthurvernet@gmail.com>
2018-10-11 09:32:00 +0200

    bs

--- A2/algorithm.c
+++ A2/algorithm.c
@@ -13,27 +13,31 @@ SCIPER      : Your SCIPER numbers
 
 void simulate(double *input, double *output, int threads, int length, int iterations)
 {
+  	omp_set_num_threads(threads);
     double *temp;
-    
-    // Parallelize this!!
-    for(int n=0; n < iterations; n++)
+    //https://software.intel.com/en-us/articles/avoiding-and-identifying-false-sharing-among-threads
+    #pragma omp parallel shared(input, output, length)
     {
-        for(int i=1; i<length-1; i++)
-        {
-            for(int j=1; j<length-1; j++)
-            {
-                    if ( ((i == length/2-1) || (i== length/2))
-                        && ((j == length/2-1) || (j == length/2)) )
-                        continue;
+      for(int n=0; n < iterations; n++)
+      {
+          #pragma omp for schedule(static)
+          for(int i=1; i<length-1; i++)
+          {
+              for(int j=1; j<length-1; j++)
+              {
+                      if ( ((i == length/2-1) || (i== length/2))
+                          && ((j == length/2-1) || (j == length/2)) )
+                          continue;
 
-                    OUTPUT(i,j) = (INPUT(i-1,j-1) + INPUT(i-1,j) + INPUT(i-1,j+1) +
-                                   INPUT(i,j-1)   + INPUT(i,j)   + INPUT(i,j+1)   +
-                                   INPUT(i+1,j-1) + INPUT(i+1,j) + INPUT(i+1,j+1) )/9;
-            }
-        }
+                      OUTPUT(i,j) = (INPUT(i-1,j-1) + INPUT(i-1,j) + INPUT(i-1,j+1) +
+                                     INPUT(i,j-1)   + INPUT(i,j)   + INPUT(i,j+1)   +
+                                     INPUT(i+1,j-1) + INPUT(i+1,j) + INPUT(i+1,j+1) )/9;
+              }
+          }
 
-        temp = input;
-        input = output;
-        output = temp;
+          temp = input;
+          input = output;
+          output = temp;
+      }
     }
 }
