60e8e042d8756c16c2a17adc829b4e0ad964d170
Simon Maulini <simon.maulini@epfl.ch>
2018-11-05 14:06:55 +0100

    simon work

--- A2/algorithm.c
+++ A2/algorithm.c
@@ -13,31 +13,50 @@ SCIPER      : Your SCIPER numbers
 
 void simulate(double *input, double *output, int threads, int length, int iterations)
 {
-  	omp_set_num_threads(threads);
     double *temp;
-    //https://software.intel.com/en-us/articles/avoiding-and-identifying-false-sharing-among-threads
-    #pragma omp parallel shared(input, output, length)
+	
+	int stepSize = length / threads;
+	
+	omp_set_num_threads(threads);
+    
+    // Parallelize this!!
+    for(int n=0; n < iterations; n++)
     {
-      for(int n=0; n < iterations; n++)
-      {
-          #pragma omp for schedule(static)
-          for(int i=1; i<length-1; i++)
-          {
-              for(int j=1; j<length-1; j++)
-              {
-                      if ( ((i == length/2-1) || (i== length/2))
-                          && ((j == length/2-1) || (j == length/2)) )
-                          continue;
+		#pragma omp parallel for collapse(2)
+		for(int stepY = 0; stepY < length; stepY += stepSize){
+			for(int stepX = 0; stepX < length; stepX += stepSize){
+				int endX = stepX + stepSize;
+				endX = (endX > length - 1) ? length-1 : endX;
+				
+				int endY = stepY + stepSize;
+				endY = (endY > length - 1) ? length-1 : endY;
+				
+				stepY = (stepY == 0) ? 1 : stepY;
+				stepX = (stepX == 0) ? 1 : stepX;
+				
+				//printf("Step : (%d,%d)\n", stepX, stepY);
+				
+				for(int y = stepY; y < endY; y++){
+					for(int x = stepX; x < endX; x++){
+						if (((x == length/2-1) || (x== length/2)) && ((y == length/2-1) || (y == length/2))){
+							continue;
+						}
+						
+						/*OUTPUT(length/2-1,length/2-1) = INIT_VALUE;
+						OUTPUT(length/2-1,length/2)   = INIT_VALUE;
+						OUTPUT(length/2,length/2-1)   = INIT_VALUE;
+						OUTPUT(length/2,length/2)     = INIT_VALUE;*/
 
-                      OUTPUT(i,j) = (INPUT(i-1,j-1) + INPUT(i-1,j) + INPUT(i-1,j+1) +
-                                     INPUT(i,j-1)   + INPUT(i,j)   + INPUT(i,j+1)   +
-                                     INPUT(i+1,j-1) + INPUT(i+1,j) + INPUT(i+1,j+1) )/9;
-              }
-          }
-
-          temp = input;
-          input = output;
-          output = temp;
-      }
+						OUTPUT(x,y) = (INPUT(x-1,y-1) + INPUT(x-1,y) + INPUT(x-1,y+1) +
+                                   INPUT(x,y-1)   + INPUT(x,y)   + INPUT(x,y+1)   +
+                                   INPUT(x+1,y-1) + INPUT(x+1,y) + INPUT(x+1,y+1))/9.0;
+					}
+				}
+			}
+		}
+		
+		temp = input;
+        input = output;
+        output = temp;
     }
 }
