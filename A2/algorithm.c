74cc61a427b056502e66bbbff3e67dcd8a484cba
Arthur Vernet <arthurvernet@gmail.com>
2018-11-06 07:37:40 +0100

    sciper

--- A2/algorithm.c
+++ A2/algorithm.c
@@ -2,7 +2,7 @@
 ============================================================================
 Filename    : algorithm.c
 Author      : Simon MAULINI & Arthur VERNET
-SCIPER      : 248115 & XXXXXX
+SCIPER      : 248115 & 245828
 
 ============================================================================
 */
@@ -14,35 +14,35 @@ SCIPER      : 248115 & XXXXXX
 void simulate(double *input, double *output, int threads, int length, int iterations)
 {
     double *temp;
-	
+
 	int stepSize = length / threads;
 	int endX, endY, tmpX, tmpY, stepY, stepX, x, y;
 	double tmpVal;
-	
+
 	omp_set_num_threads(threads);
-    
-    for(int n=0; n < iterations; n++)
-    {
+
+    for(int n = 0; n < iterations; n++){
+
 		#pragma omp parallel for collapse(2)
 		for(stepY = 0; stepY < length; stepY += stepSize){
 			for(stepX = 0; stepX < length; stepX += stepSize){
-				
+
 				//Check if the end of the step is out of bound
 				endX = stepX + stepSize;
 				endX = (endX > length - 1) ? length-1 : endX;
 				endY = stepY + stepSize;
 				endY = (endY > length - 1) ? length-1 : endY;
-				
+
 				//Ignore the 0 coordonates
 				tmpY = (stepY == 0) ? 1 : stepY;
 				tmpX = (stepX == 0) ? 1 : stepX;
-				
+
 				for(y = tmpY; y < endY; y++){
 					for(x = tmpX; x < endX; x++){
 						if (((x == length/2-1) || (x== length/2)) && ((y == length/2-1) || (y == length/2))){
 							continue;
 						}
-						
+
 						//We slit the initial one line instruction to
 						// multiple line to be more readable
 						tmpVal = 0.0;
@@ -54,9 +54,9 @@ void simulate(double *input, double *output, int threads, int length, int iterat
 				}
 			}
 		}
-		
+
 		temp = input;
-        input = output;
-        output = temp;
+    input = output;
+    output = temp;
     }
 }
